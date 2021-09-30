c453ad7545468cce01d144b471d876cc6f360d63
tiagosantosarxi <tiago.santos@arxi.pt>
2021-09-30 17:35:31 +0100

    Aditional routes

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -3,12 +3,12 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: True }
+    value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: True },
+    value: { type: Number, min: 0, required: true },
     status: { type: String, required: true, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO']},
 })
 
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "The attribute '{PATH}' is required."
