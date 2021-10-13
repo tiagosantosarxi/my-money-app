5f74d5e389a3265e872715e734496724485f59fa
tiagosantosarxi <tiago.santos@arxi.pt>
2021-10-14 00:46:47 +0100

    Add tabs

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title="Ciclo de Pagamento" small="Registo" />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label="Listar" icon="bars" target="tabList" />
+                            <TabHeader label="Adicionar" icon="plus" target="tabCreate" />
+                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+                            <TabHeader label="Eliminar" icon="trash-o" target="tabDelete" />
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
